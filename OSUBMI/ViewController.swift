70b837d026fe44eda8b45e36a5e3b4426117c983
tahatopiwala <taha.topiwala@icloud.com>
2016-04-06 01:07:02 -0400

    Initial setup

---
+++ OSUBMI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OSUBMI
+//
+//  Created by Taha Topiwala on 4/6/16.
+//  Copyright © 2016 Taha Topiwala. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
